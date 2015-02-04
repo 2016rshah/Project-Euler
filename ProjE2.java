a6cbf8a0cbcbac7c45013af7f14e09e5de3930ca
Rushi Shah <2016rshah@gmail.com>
2015-02-03 22:13:36 -0500

    Solution to problem 2

---
+++ ProjE2.java
@@ -0,0 +1,16 @@
+public class ProjE2{
+	public static void main(String[] args){
+		int a = 1;
+		int b = 2;
+		int total = b; //make sure you catch initial value of b=2
+		while(b<4000000){
+			int newB = a+b;
+			a = b;
+			b = newB;
+			if(b%2==0){
+				total+=b;
+			}
+		}
+		System.out.println("Total is: " + total);
+	}
+}
