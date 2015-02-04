9ce091ef51afd3dcdac063f5abd9356968ec3bef
Rushi Shah <2016rshah@gmail.com>
2015-02-03 22:08:56 -0500

    Solution to problem 1

---
+++ ProjE1.java
@@ -0,0 +1,11 @@
+public class ProjE1{
+	public static void main(String[] args){
+		int total = 0;
+		for(int i = 0; i<1000; i++){
+			if(i%3==0 || i%5==0){
+				total+=i;
+			}
+		}
+		System.out.println("Final total was: " + total);	
+	}
+}
