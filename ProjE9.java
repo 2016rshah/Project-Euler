33fadd5e1cf109a1807957daa3355ca2901ae8e4
Rushi Shah <2016rshah@gmail.com>
2015-02-05 17:15:14 -0500

    Solution to problem 9

---
+++ ProjE9.java
@@ -0,0 +1,14 @@
+public class ProjE9{
+	public static void main(String[] args){
+		for(int b = 1; b<1000; b++){
+			for(int a = 1; a<b; a++){
+				if(Math.sqrt(a*a+b*b)%1 == 0){
+					int c = (int)(Math.sqrt(a*a+b*b));
+					if(a+b+c == 1000){
+						System.out.println("Answer: "+(a*b*c));
+					}
+				}
+			}
+		}
+	}
+}
