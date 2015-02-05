ecc4fde3fbe725c6c454542c1a91411c70fe2a39
Rushi Shah <2016rshah@gmail.com>
2015-02-04 20:08:11 -0500

    Code cleanup for problem 7

--- ProjE7.java
+++ ProjE7.java
@@ -1,24 +1,18 @@
-import java.util.ArrayList;
 public class ProjE7{
 	public static void main(String[] args){
 		int numPrimesFound = 1;
-		int currPrime = 2;
 		int currNum = 2;
 		while(numPrimesFound<=10001){
-			if(isPrime(currNum)){
-				currPrime = currNum;
+			if(isPrime(currNum))
 				numPrimesFound++;
-			}
 			currNum++;
 		}
-		System.out.println("Answer: "+currPrime);
+		System.out.println("Answer: "+(currNum-1));
 	}
 	public static boolean isPrime(long num){
-		for(int i = 2; i<(int)(Math.sqrt(num))+1; i++){ //Sqrt speeds it way up. Also, round up so add one
-			if(num%i == 0){
+		for(int i = 2; i<(int)(Math.sqrt(num))+1; i++) //Sqrt speeds it way up. Also, round up so add one
+			if(num%i == 0)
 				return false;
-			}
-		}
 		return true;
 	}
 }
