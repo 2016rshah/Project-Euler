c288babc51b44721daca3290120e4b0f727ef71c
Rushi Shah <2016rshah@gmail.com>
2015-04-20 00:50:44 -0400

    Cut isPrime calculation time in half

--- Haskell/ProjE07.hs
+++ Haskell/ProjE07.hs
@@ -1,5 +1,6 @@
-isPrime :: Int -> Bool
-isPrime a = [] == [x | x <- [2..a-1], a `mod` x == 0]
+isPrime :: Integer -> Bool
+isPrime 2 = True
+isPrime a = [] == [x | x <- [2..(a `div` 2) + 1], a `mod` x == 0]
 
 primes :: Int -> [Integer]
 primes n = take n [(fromIntegral i) | i <-[2..], isPrime i]
