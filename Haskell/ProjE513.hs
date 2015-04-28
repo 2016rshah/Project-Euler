f5ff429a3b79b3483e0488aa8867bafc4307f5a1
Rushi Shah <2016rshah@gmail.com>
2015-04-28 11:41:26 -0400

    Does not compile yet

--- Haskell/ProjE513.hs
+++ Haskell/ProjE513.hs
@@ -7,11 +7,16 @@ median [a,b,c] = (sqrt (2*a*a+2*b*b-c*c)) / 2
 integralMedian :: (RealFrac b, Floating b) => [b] -> Bool
 integralMedian xs = isInt (median xs) 4
 
-perms :: (Ord t, Num t, Enum t) => t -> [[t]]
-perms n = [[a, b, c] | c <- [2,4..n], b <- [1..c], a <- [1..b], a+b > c, a+c > b]
+perms :: (Ord t, Num t, Enum t, Integral t) => t -> [[t]]
+perms n = [[a, b, c] | 
+			c <- [2,4..n], 
+			b <- [1..c], 
+			if b `mod`2 == 0 then a <- [2, 4 ..b] else a <- [1, 3 ..b],
+			a+b > c, 
+			a+c > b]
 
-solutions :: (RealFrac b, Floating b, Enum b) => b -> [[b]]
+solutions :: (RealFrac b, Floating b, Enum b, b) => b -> [[b]]
 solutions n = [xs | xs <- (perms n), integralMedian xs]
 
-main = print $ length $ solutions 500
+main = print $ length $ solutions 50
 
