c59b5b598307ce8009f50db8e20c8631fc5166de
Rushi Shah <2016rshah@gmail.com>
2015-05-07 11:50:33 -0400

    Improve problem 14: Haskell

--- Haskell/ProjE14.hs
+++ Haskell/ProjE14.hs
@@ -3,10 +3,10 @@ import Data.List
 hailstone :: Integer -> Integer
 hailstone 1 = 1
 hailstone n 
-	| n `mod` 2 == 0		= 1 + hailstone (n `div` 2)
-	| otherwise				= 1 + hailstone (3 * n + 1)
+	| n `mod` 2	== 0	= 1 + hailstone (n `div` 2)
+	| otherwise			= 1 + hailstone (3 * n + 1)
 
 hailstones :: Integer -> [(Integer, Integer)]
-hailstones n = [(hailstone x, x) | x <- [1..n]]
+hailstones n = [(hailstone x, x) | x <- [1, 3 .. n]]
 
 main = print $ snd $ last $ sort $ hailstones 1000000
